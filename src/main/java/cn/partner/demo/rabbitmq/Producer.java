4880590ff6b6670ee99f32fad368e2086022a205
Yonsin Joe <qiao.yongxin@navercorp.com>
2016-07-22 13:22:37 +0800

    transaction

--- src/main/java/cn/partner/demo/rabbitmq/Producer.java
+++ src/main/java/cn/partner/demo/rabbitmq/Producer.java
@@ -1,5 +1,7 @@
 package cn.partner.demo.rabbitmq;
 
+import java.io.IOException;
+
 import cn.partner.demo.rabbitmq.RabbitmqUtils.RabbitConstants;
 
 import com.rabbitmq.client.Channel;
@@ -14,16 +16,30 @@ public class Producer {
 
     public static void send(String message) {
         Connection conn = RabbitmqUtils.getConn();
+        Channel channel = null;
         try {
             if (!conn.isOpen()) {
                 conn = RabbitmqUtils.getConn();
             }
-            Channel channel = conn.createChannel();
+            channel = conn.createChannel();
+            // Transaction begin
+            channel.txSelect();
             channel.basicPublish(RabbitConstants.EXCHANGE_NAME, RabbitConstants.ROUTING_KEY, false,
                     MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
             System.out.println(String.format("+++send message : %s, channel : %s", message, channel));
+            // when test transaction
+            // throw new RuntimeException();
+
+            // Transaction commit
+            channel.txCommit();
         } catch (Exception e) {
             e.printStackTrace();
+            try {
+                // Transaction rollback
+                channel.txRollback();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
         }
     }
 }
